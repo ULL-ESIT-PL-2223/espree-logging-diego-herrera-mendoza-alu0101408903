2baa8547dcfc8e041c146fab0fe70f2e1fe25b4d
Diego Herrera <alu0101408903@ull.edu.es>
2023-02-28 18:53:35 +0000

    Hecha

--- bin/log.js
+++ bin/log.js
@@ -4,14 +4,18 @@ import { program } from "commander";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const { version } = require("../package.json");
-import { transpile } from "../src/logging-espree.js";
+import { addLogging, transpile } from "../src/logging-espree.js";
+
 
 program
   .version(version)
   .argument("<filename>", 'file with the original code')
   .option("-o, --output <filename>", "file in which to write the output")
   .action((filename, options) => {
-    transpile(filename, options.output);
+      transpile(filename, options.output);
   });
 
 program.parse(process.argv);
+
+
+
--- src/logging-espree.js
+++ src/logging-espree.js
@@ -3,14 +3,47 @@ import * as espree from "espree";
 import * as estraverse from "estraverse";
 import * as fs from "fs/promises";
 
+function getLineOffsets(str) {
+  const regex = /\r?\n/g;
+  const offsets = [0];
+  while (regex.test(str)) offsets.push(regex.lastIndex);
+  offsets.push(str.length);
+  return offsets;
+}
+
 export async function transpile(inputFile, outputFile) {
-  // Fill in the code here
+  let input = await fs.readFile(inputFile, 'utf-8')
+  let output = addLogging(input);
+  if (outputFile === undefined) {
+      console.log(output);
+      return;
+  }
+  await fs.writeFile(outputFile, output);
 }
 
 export function addLogging(code) {
-  // Fill in the code here
+  let lineStarts = getLineOffsets(code);
+  let lineNum = 0;   
+  const ast = espree.parse(code, { ecmaVersion: 6 });
+  estraverse.traverse(ast, {
+    enter: function (node, parent) {
+      if (node.type === 'FunctionDeclaration' ||
+        node.type === 'FunctionExpression' || node.type === 'ArrowFunctionExpression') {
+          while (lineStarts[lineNum] < node.body.body[0].start) lineNum++;
+          addBeforeCode(node, lineNum - 1);
+      }
+    }
+  });
+  return escodegen.generate(ast);
 }
 
-function addBeforeCode(node) {
- // Fill in the code here
+function addBeforeCode(node, lineNum) {
+  let name = node.id ? node.id.name : '<anonymous function>';
+  let params = node.params.map(function (argumentos) {
+    return "${ " + argumentos.name + " }";
+  });
+  params = params.join(', ');
+  let beforeCode = `console.log(\`Entering ${name}(${params}) at line ${lineNum}\`);`;
+  let beforeNodes = espree.parse(beforeCode, { ecmaVersion: 6 }).body;
+  node.body.body = beforeNodes.concat(node.body.body);
 }
